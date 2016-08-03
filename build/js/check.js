b66139967d9cb37a07d3b4eade7e402e67658519
MammaSonnim <mammasonnim@users.noreply.github.com>
2016-08-03 03:27:36 +0300

    fix codestyle

--- build/js/check.js
+++ build/js/check.js
@@ -1,37 +1,26 @@
 'use strict';
 
 function getMessage(a, b) {
-    var message;
-    var numberOfSteps;
-    var distancePath;
+  var numberOfSteps;
+  var distancePath;
+  var arr = [];
 
-    if ( typeof a == 'boolean' ) {
-        message = a ? 'Я попал в ' + b : 'Я никуда не попал';
-
-    } else if ( typeof a == 'number' ) {
-        message = 'Я прыгнул на ' + (a * 100) + ' сантиметров';
-
-    } else if ( typeof a == 'object' && !(typeof b == 'object') ) {
-        numberOfSteps = a.reduce(function(a, b) {
-            return a + b;
-        });
-        message = 'Я прошёл ' + numberOfSteps + ' шагов';
-
-    } else if ( typeof a == 'object' && typeof b == 'object' ) {
-        var arr = [];
-
-        for (var i=0; i < a.length; i++) {
-            arr.push( a[i] * b[i] );
-
-        }
-
-        distancePath = arr.reduce(function(a, b) {
-            return a + b;
-        });
-
-        message = 'Я прошёл ' + distancePath + ' метров';
+  if (typeof a === 'boolean') {
+    return a ? 'Я попал в ' + b : 'Я никуда не попал';
+  } else if (typeof a === 'number') {
+    return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
+  } else if (Array.isArray(a) && !Array.isArray(b)) {
+    numberOfSteps = a.reduce(function (prev, next) {
+      return prev + next;
+    });
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
+  } else if (Array.isArray(a) && Array.isArray(b)) {
+    for (var i = 0; i < a.length; i++) {
+      arr.push(a[i] * b[i]);
     }
-
-    return message;
-
-};
+    distancePath = arr.reduce(function (prev, next) {
+      return prev + next;
+    });
+    return 'Я прошёл ' + distancePath + ' метров';
+  }
+}
