88f7e52ca6a51501dd8a98f9e5a821463f20b051
MammaSonnim <mammasonnim@users.noreply.github.com>
2016-08-04 18:14:31 +0300

    :fire: rm map, fix reduce, fix else if

--- build/js/check.js
+++ build/js/check.js
@@ -3,34 +3,25 @@
 function getMessage(a, b) {
   var numberOfSteps;
   var distancePath;
-  var arr = [];
 
   if (typeof a === 'boolean') {
     return a ? 'Я попал в ' + b : 'Я никуда не попал';
-  } else if (typeof a === 'number') {
+  }
+  if (typeof a === 'number') {
     return 'Я прыгнул на ' + (a * 100) + ' сантиметров';
-  } else if (Array.isArray(a) && !Array.isArray(b)) {
+  }
+  if (Array.isArray(a) && !Array.isArray(b)) {
     numberOfSteps = a.reduce(function(prev, current) {
-      if (typeof current !== 'undefined') {
-        return prev + current;
-      } else {
-        return false;
-      }
-    });
-    return 'Я прошёл ' + numberOfSteps + ' шагов';
-  } else if (Array.isArray(a) && Array.isArray(b)) {
-    arr = a.map(function(value, i) {
-      if (typeof b[i] !== 'undefined') {
-        return value * b[i];
-      } else {
-        return false;
-      }
-    });
-    distancePath = arr.reduce(function(prev, current) {
       return prev + current;
     });
-    return 'Я прошёл ' + distancePath + ' метров';
-  } else {
-    return false;
+    return 'Я прошёл ' + numberOfSteps + ' шагов';
   }
+  if (Array.isArray(a) && Array.isArray(b)) {
+    distancePath = a.reduce(function(prev, current, index) {
+      console.log(current);
+      return prev + current * (b[index] || 0);
+    }, 0);
+    return 'Я прошёл ' + distancePath + ' метров';
+  }
+  return false;
 }
