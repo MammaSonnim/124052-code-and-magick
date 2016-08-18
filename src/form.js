ecd2cb6d12903728485acb3ccff88cf5c02d45d5
MammaSonnim <mammasonnim@users.noreply.github.com>
2016-08-18 13:35:30 +0300

    :ambulance: fix rm attribute stx, fix code style, rm call of toggle-fn from validate-fn

--- src/form.js
+++ src/form.js
@@ -4,7 +4,7 @@ window.form = (function() {
   var STARS_MIN = 3;
   var formContainer = document.querySelector('.overlay-container');
   var reviewForm = document.querySelector('.review-form');
-  var formStars = reviewForm.elements['review-mark'];
+  var formMark = reviewForm.elements['review-mark'];
   var formCloseButton = reviewForm.querySelector('.review-form-close');
   var formSubmitButton = reviewForm.querySelector('.review-submit');
   var nameInput = reviewForm.elements['review-name'];
@@ -47,27 +47,26 @@ window.form = (function() {
     },
 
     enable: function() {
-      if (form.isDisabled) {
-        form.isDisabled = false;
-        formSubmitButton.removeAttribute('disabled', 'disabled');
-        formIndicators.classList.add('invisible');
+      if (!this.isDisabled) {
+        return;
       }
+      this.isDisabled = false;
+      formSubmitButton.removeAttribute('disabled');
+      formIndicators.classList.add('invisible');
     },
 
-    toggleRequired: function(target, source, minValue) {
-      if (source && minValue) {
-        var rating = source.value;
+    toggleInputRequired: function(target, mark, minValue) {
+      if (mark && minValue) {
+        var rating = mark.value;
         if (rating < minValue) {
           textInput.setAttribute('required', 'required');
         } else {
-          textInput.removeAttribute('required', 'required');
+          textInput.removeAttribute('required');
         }
       }
     },
 
-    validateInput: function(input, indicator, source, minValue) {
-      this.toggleRequired(input, source, minValue);
-
+    validateInput: function(input, indicator) {
       if (input.hasAttribute('required')) {
         if (input.value) {
           indicator.classList.add('invisible');
@@ -86,14 +85,15 @@ window.form = (function() {
     }
   };
 
-  reviewForm.onclick = function() {
+  reviewForm.onchange = function() {
+    form.toggleInputRequired(textInput, formMark, STARS_MIN);
     form.validateInput(nameInput, nameIndicator);
-    form.validateInput(textInput, textIndicator, formStars, STARS_MIN);
+    form.validateInput(textInput, textIndicator);
   };
 
   reviewForm.oninput = function() {
     form.validateInput(nameInput, nameIndicator);
-    form.validateInput(textInput, textIndicator, formStars, STARS_MIN);
+    form.validateInput(textInput, textIndicator);
   };
 
   reviewForm.onsubmit = function(evt) {
