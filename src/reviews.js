715dd24f8ad314076d1acff51830493ace922e76
Mamma Sonnim <msonnim@gmail.com>
2016-09-08 14:06:35 +0300

    :construction: fix structure, rm switch

--- src/reviews.js
+++ src/reviews.js
@@ -2,38 +2,41 @@
 
 window.CallbackRegistry = {};
 var REVIEWS_LOAD_URL = 'http://localhost:1506/api/reviews?callback=CallbackRegistry.{name}';
+var AUTHOR_IMG_SIZE = 124;
+var IMG_LOAD_TIMEOUT = 10000;
 var reviewsContainer = document.querySelector('.reviews-list');
 var reviewsFilter = document.querySelector('.reviews-filter');
 var template = document.getElementById('review-template');
 var reviewSource = (template.content || template).querySelector('.review');
+var ratingMap = ['two', 'three', 'four', 'five'];
 
 var jsonp = function(url, cb) {
   var callbackName = 'cb' + Date.now();
   var script = document.createElement('script');
-  script.src = REVIEWS_LOAD_URL.replace('{name}', callbackName);
 
   document.body.appendChild(script);
 
   window.CallbackRegistry[callbackName] = function(data) {
     cb(data);
+  };
+
+  script.onload = script.onerror = function() {
     delete window.CallbackRegistry[callbackName];
     document.body.removeChild(script);
   };
+
+  script.src = REVIEWS_LOAD_URL.replace('{name}', callbackName);
 };
 
-var getReviewElement = function(review) {
-  var AUTHOR_IMG_SIZE = 124;
-  var IMG_LOAD_TIMEOUT = 10000;
+var createReviewElement = function(review) {
   var reviewElement = reviewSource.cloneNode(true);
-  var reviewRatingClassname = translateNumToString(review.rating);
   var reviewAuthor = reviewElement.querySelector('.review-author');
   var image = new Image();
 
   reviewElement.querySelector('.review-text').textContent = review.description;
-  if (reviewRatingClassname !== null) {
-    reviewElement.querySelector('.review-rating').classList.add('review-rating-' + reviewRatingClassname);
+  if (ratingMap[review.rating]) {
+    reviewElement.querySelector('.review-rating').classList.add('review-rating-' + ratingMap[review.rating - 2]);
   }
-
   reviewAuthor.title = review.author.name;
   reviewAuthor.alt = review.author.name;
 
@@ -62,37 +65,14 @@ var getReviewElement = function(review) {
 };
 
 var renderReviews = function(data) {
-  reviewsFilter.classList.add('invisible');
   window.reviews = data;
   window.reviews.forEach(function(review) {
-    reviewsContainer.appendChild(getReviewElement(review));
+    reviewsContainer.appendChild(createReviewElement(review));
   });
   reviewsFilter.classList.remove('invisible');
 };
 
-var translateNumToString = function(number) {
-  var result;
-
-  switch (number) {
-    case 2:
-      result = 'two';
-      break;
-    case 3:
-      result = 'three';
-      break;
-    case 4:
-      result = 'four';
-      break;
-    case 5:
-      result = 'five';
-      break;
-    default:
-      result = null;
-  }
-  return result;
-};
-
-jsonp(REVIEWS_LOAD_URL, renderReviews);
-
-
-
+(function() {
+  reviewsFilter.classList.add('invisible');
+  jsonp(REVIEWS_LOAD_URL, renderReviews);
+})();
